fc36b9ec672410a89dd5a4a18b0a385deece0c40
Stephen Cleary <github.ourteddybear@xoxy.net>
2020-05-22 08:18:43 -0400

    build

--- dist/index.js
+++ dist/index.js
@@ -995,10 +995,13 @@ const glob = __webpack_require__(281);
 const run = async () => {
   try {
     const patterns = core.getInput('patterns', { required: true });
-    console.log(`Hello ${patterns}!`);
+    console.log(`Using pattern ${patterns}`);
 
     const globber = await glob.create(patterns);
     const files = await globber.glob();
+    for (const file in files) {
+        console.log(`Found ${file}`);
+    }
     core.setOutput("files", files);
   } catch (error) {
     core.setFailed(error.message);
